997320465b2b4363bfa3dec153bd0152bd091d6a
tsanevp <tsanev-pavel@etu.univ-savoie.fr>
2025-09-19 16:23:56 +0200

    Mapper, truc de plouck

---
+++ R5A08_TP1/Models/DTO/MarqueDto.cs
@@ -0,0 +1,9 @@
+﻿namespace R5A08_TP1.Models.DTO
+{
+    public class MarqueDto
+    {
+        public int Id { get; set; }
+        public string? Nom { get; set; }
+        public int NbProduits { get; set; }
+    }
+}
---
+++ R5A08_TP1/Models/Mapper/ProduitMapper.cs
@@ -0,0 +1,30 @@
+﻿using R5A08_TP1.Models.DTO;
+using R5A08_TP1.Models.EntityFramework;
+
+namespace R5A08_TP1.Models.Mapper
+{
+    public class ProduitMapper : IMapper<Produit, ProduitDto>
+    {
+        public Produit? FromDTO(ProduitDto dto)
+        {
+            return new Produit()
+            {
+                IdProduit = dto.Id,
+                NomProduit = dto.Nom,
+                TypeProduitAssoc = new TypeProduit() { NomTypeProduit = dto.Type },
+                MarqueAssoc = new Marque() { NomMarque = dto.Marque }
+            };
+        }
+
+        public ProduitDto? FromEntity(Produit entity)
+        {
+            return new ProduitDto()
+            {
+                Id = entity.IdProduit,
+                Nom = entity.NomProduit,
+                Type = entity.TypeProduitAssoc?.NomTypeProduit,
+                Marque = entity.MarqueAssoc?.NomMarque
+            };
+        }
+    }
+}
